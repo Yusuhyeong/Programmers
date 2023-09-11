73f39db497eae3121dcf7445b04ebc5697102895
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2023-09-11 09:43:53 +0900

    Create Solution.java

---
+++ lv1/나머지가 1이 되는 수 찾기/Solution.java
@@ -0,0 +1,41 @@
+/*
+문제 설명
+자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+제한사항
+N의 범위 : 100,000,000 이하의 자연수
+
+입출력 예
+N	answer
+123	6
+987	24
+*/
+
+
+package lv1;
+
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		int testcase = 10;
+		Solution solution = new Solution();
+		System.out.println(solution.solution(testcase));
+	}
+	
+	public int solution(int n) {
+        int answer = 1;
+        
+        while(true){
+            if(n % answer == 1){
+                break; 
+            }
+            else{
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
