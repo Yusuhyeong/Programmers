4bea891e9b613c2642651cc128640971d486777a
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2023-09-15 09:18:21 +0900

    lv1 풀이

---
+++ lv1/최대공약수와 최소공배수/Solution.java
@@ -0,0 +1,45 @@
+/*
+문제 설명
+두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요. 
+배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다. 
+예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
+
+제한 사항
+두 수는 1이상 1000000이하의 자연수입니다.
+
+입출력 예
+n	m	return
+3	12	[3, 12]
+2	5	[1, 10]
+*/
+
+
+package lv1;
+
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		int testcaseA = 3;
+		int testcaseB = 12;
+		Solution solution = new Solution();
+		System.out.println(Arrays.toString(Solution.solution(testcaseA, testcaseB)));
+	}
+	
+	public static long[] solution(int n, int m) {
+        long[] answer = new long [2];
+
+        if(m%n==0){
+            answer[0] = n;
+            answer[1] = m;
+        }else{
+            for(int i = 1; i <= m; i++){
+                if(n%i==0 && m%i==0){
+                    answer[0] = i;
+                }
+            }
+            answer[1] = n*m/answer[0];
+        }
+        return answer;
+    }
+}
