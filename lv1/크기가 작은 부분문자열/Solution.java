9c13c9fd8d6b35a2ffd7576bc86d1d79ef280693
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2023-09-18 08:36:49 +0900

    lv1풀이

---
+++ lv1/크기가 작은 부분문자열/Solution.java
@@ -0,0 +1,54 @@
+/*
+문제 설명
+숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서, 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는 함수 solution을 완성하세요.
+
+예를 들어, t="3141592"이고 p="271" 인 경우, t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다. 
+이 문자열이 나타내는 수 중 271보다 작거나 같은 수는 141, 159 2개 입니다.
+
+제한사항
+1 ≤ p의 길이 ≤ 18
+p의 길이 ≤ t의 길이 ≤ 10,000
+t와 p는 숫자로만 이루어진 문자열이며, 0으로 시작하지 않습니다.
+
+입출력 예
+t				p		result
+"3141592"		"271"	2
+"500220839878"	"7"		8
+"10203"			"15"	3
+*/
+
+
+package lv1;
+
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		String testcaseA = "3141592";
+		String testcaseB = "271";
+		Solution solution = new Solution();
+		System.out.println(Solution.solution(testcaseA, testcaseB));
+	}
+	
+	public static int solution(String t, String p) {
+        int answer = 0;
+        // ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < t.length(); i++) {
+            if(i+p.length() > t.length()) {
+                break;
+            }
+            
+            if(Long.parseLong(t.substring(i, i + p.length())) <= Long.parseLong(p)) {
+                // System.out.println(Integer.parseInt(t.substring(i, i + p.length())));
+                answer++;
+            }
+            
+            // list.add(Integer.parseInt(t.substring(i, i + p.length())));
+        }
+        
+        // System.out.println(list);
+        
+        return answer;
+    }
+}
