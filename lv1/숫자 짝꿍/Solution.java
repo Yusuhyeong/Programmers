6f58cc4ddd3df5f1ff4a06a7068a68c9111ba3f9
Yusuhyeong <63995343+Yusuhyeong@users.noreply.github.com>
2023-09-25 08:43:36 +0900

    lv1 풀이

---
+++ lv1/숫자 짝꿍/Solution.java
@@ -0,0 +1,83 @@
+/*
+문제 설명
+두 정수 X, Y의 임의의 자리에서 공통으로 나타나는 정수 k(0 ≤ k ≤ 9)들을 이용하여 만들 수 있는 가장 큰 정수를 두 수의 짝꿍이라 합니다(단, 공통으로 나타나는 정수 중 서로 짝지을 수 있는 숫자만 사용합니다). 
+X, Y의 짝꿍이 존재하지 않으면, 짝꿍은 -1입니다. X, Y의 짝꿍이 0으로만 구성되어 있다면, 짝꿍은 0입니다.
+
+예를 들어, X = 3403이고 Y = 13203이라면, X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 3, 0, 3으로 만들 수 있는 가장 큰 정수인 330입니다. 
+다른 예시로 X = 5525이고 Y = 1255이면 X와 Y의 짝꿍은 X와 Y에서 공통으로 나타나는 2, 5, 5로 만들 수 있는 가장 큰 정수인 552입니다(X에는 5가 3개, Y에는 5가 2개 나타나므로 남는 5 한 개는 짝 지을 수 없습니다.)
+두 정수 X, Y가 주어졌을 때, X, Y의 짝꿍을 return하는 solution 함수를 완성해주세요.
+
+제한사항
+3 ≤ X, Y의 길이(자릿수) ≤ 3,000,000입니다.
+X, Y는 0으로 시작하지 않습니다.
+X, Y의 짝꿍은 상당히 큰 정수일 수 있으므로, 문자열로 반환합니다.
+
+입출력 예
+X			Y			result
+"100"		"2345"		"-1"
+"100"		"203045"	"0"
+"100"		"123450"	"10"
+"12321"		"42531"		"321"
+"5525"		"1255"		"552"
+*/
+
+
+package lv1;
+
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		String testcaseA = "100";
+		String testcaseB = "2345";
+		Solution solution = new Solution();
+		System.out.println(solution.solution(testcaseA, testcaseB));
+	}
+	
+	public static String solution(String X, String Y) {
+        ArrayList<Integer> list =  new ArrayList<>();
+        
+        int[] xNum = new int[10];
+        for(int i = 0; i < X.length(); i++) {
+            xNum[Character.getNumericValue(X.charAt(i))]++;
+        }
+        
+        // System.out.println(Arrays.toString(xNum));
+        
+        int[] yNum = new int[10];
+        for(int i = 0; i < Y.length(); i++) {
+            yNum[Character.getNumericValue(Y.charAt(i))]++;
+        }
+        
+        // System.out.println(Arrays.toString(yNum));
+        
+        int[] num = new int[10];
+        for(int i = xNum.length - 1; i >= 0; i--) {
+            if(xNum[i] > 0 && yNum[i] > 0) {
+                if(xNum[i] <= yNum[i]) {
+                    num[i] = xNum[i];
+                }
+                else {
+                    num[i] = yNum[i];
+                }
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        for(int i=0; i<10; i++){
+            cnt += num[i];
+            if(num[i] > 0){
+                for(int j=0; j<num[i]; j++){
+                    sb.append(i);
+                }
+            }
+        }
+
+        String answer = sb.reverse().toString();
+        
+        if(cnt == num[0]) answer = "0";
+
+        return cnt > 0 ? answer : "-1";
+    }
+}
